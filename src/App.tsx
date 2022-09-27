0692da0f112d964803bf2713b7b78f060fc5650d
Alexander Popelnyk <popelnyk@icloud.com>
2022-09-27 22:45:51 +0200

    init app with create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      Test
+    </div>
+  );
+}
+
+export default App;
